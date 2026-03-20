2f3196a19adda04b383ece3472fd532c92fb4215
Karim <Karimisma6464@Gmail.com>
2026-03-20 23:36:11 +0100

    feat(builds): add Fushu, Rebecca, Lisbell, Psyche builds (10/22)
    - Fushu (Hydro Support): Skill DMG support with Wings·Volition stack
    - Rebecca (Hydro DPS): Summon DPS with Aurelia Aurita jellyfish
    - Lisbell (Lumino DPS): Consonance Weapon Samael focused
    - Psyche (Anemo DPS): Ultimate DPS with Flamboyance state

--- src/data/characters/builds/index.ts
+++ src/data/characters/builds/index.ts
@@ -4,6 +4,10 @@ import charXibi from "./char-xibi.json";
 import charLandi from "./char-landi.json";
 import charFeina from "./char-feina.json";
 import charNifu from "./char-nifu.json";
+import charBaiheng from "./char-baiheng.json";
+import charShuimu from "./char-shuimu.json";
+import charBaonu from "./char-baonu.json";
+import charSaiqi from "./char-saiqi.json";
 
 // Add new character builds here:
 // 1. import charXxx from "./char-xxx.json";
@@ -16,4 +20,8 @@ export const allBuilds = [
   ...charLandi,
   ...charFeina,
   ...charNifu,
+  ...charBaiheng,
+  ...charShuimu,
+  ...charBaonu,
+  ...charSaiqi,
 ];
